37ffcbaad324196341304b7efc94d177182c9b21
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-03-01 13:06:58 -0800

    added post section to notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 3001;
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-//const unique = require('uniqid');
+const unique = require('uniqid');
 const { words } = require('./db/db.json');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
@@ -12,6 +12,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(express.json());
 
+/////start of get section
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
@@ -25,15 +26,20 @@ app.get('/api/notes/:id', (req, res) => {
         res.send(404);
       }
 });
+/////end of get section
+
+/////post section
+app.post('/api/notes', (req, res) => {
+    req.body.id = uniqid();
+    const notes = createNewNote(req.body, words);
+      res.json(notes);
+});
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);

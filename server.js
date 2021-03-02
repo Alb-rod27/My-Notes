4db39ae1f95b76f54ac2f4e6b6be4aa9ead5f137
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-03-01 18:46:44 -0800

    fixing note to appear

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const uniqid = require('uniqid');
 const { words } = require('./db/db.json');
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
@@ -13,11 +12,15 @@ app.use(express.static('public'));
 app.use(express.json());
 
 /////start of get section
+app.get('/api/notes', (req, res) => {
+    res.json(words);
+});
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/', (req, res) => {
     const result = findById(req.params.id, words);
       if(result){
         res.json(result);
@@ -35,11 +38,47 @@ app.post('/api/notes', (req, res) => {
       res.json(notes);
 });
 
+//api post route for notes endpoint
+// app.post('/api/notes', (req, res) => {
+//     // console.log(dbnotes)
+//     //req.body is where out incoming content will be. get unique id
+//     req.body.id = dbnotes.length.toString();
+//     const note = req.body;
+//     dbnotes.push(note);
+//     //write note json - stringify converts json and keeps data formatted
+//     fs.writeFileSync(
+//         path.join(__dirname, './db/db.json'),
+//         JSON.stringify(dbnotes, null, 2)
+//     );
+//     res.json(note);//or res.json(req.body)
+// });
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.post('/api/notes', (req, res) => {
+    req.body.id = uniqid();
+    const notes = createNewNote(req.body, words);
+      res.json(notes);
+});
+
+function createNewNote(body, dbNotes) {
+    const note = body;
+    dbNotes.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ words: dbNotes }, null, 2)
+    );
+    return note;
+}
+
+function findById(id, dbNotes) {
+    const result = dbNotes.filter(words => words.id === id)[0];
+      return result;
+}
+
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
